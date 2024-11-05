000022d00ed944470d013f3efff3cf2c00c2c910
Skailodon <jackiehtruong@outlook.com>
2024-11-04 20:09:11 -0800

    all page content

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Country from './components/Country'
 
 function App() {
   const DATA_URL = "https://restcountries.com/v3.1/all"
   const [data, setData] = useState([])
 
-  useEffect(() => {
-
-  })
+  useEffect((() => {
+    fetchData()
+  }), [])
 
   async function fetchData() {
     try {
@@ -20,141 +21,87 @@ function App() {
     } catch (error) {
       console.log(error)
       console.log("error message")
-      setErrorDisplay(<p>Error Message!</p>)
     }
+  }
+
+  // https://stackoverflow.com/questions/35976167/find-unique-values-from-an-array-in-react-js
+  const uniqueRegions = Array.from(new Set(data.map((country) => country.region)));
+
+  const uniqueSubregions = Array.from(new Set(data.map((country) => country.subregion)));
+
+  function alphaHandler() {
+    alert("handler")
+  }
+
+  function populationHandler() {
+    alert("handler")
+  }
+
+  function areaHandler() {
+    alert("handler")
   }
 
   return (
     <div className='home'>
-      <div className='m-auto'>
-        <h1 className='font-black text-5xl'>Countries of the World</h1>
-        <div className='flex mt-6 mb-6'>
-          <div className='border-2 border-black m p-3 flex items-center space-x-3'>
+      <span>
+      </span>
+      <h1 className='m-auto font-black text-5xl'>Countries of the World</h1>
+      <div className='m-auto flex mt-6 mb-6'>
+        <div className='border-2 border-black m p-3 flex items-center space-x-3'>
+          <input type='checkbox' />
+          <p>Alpha</p>
+        </div>
+
+        <div className='border-2 border-black m p-3 flex flex-col space-x-3'>
+          <p>Top 10</p>
+          <div className='flex space-x-6'>
             <input type='checkbox' />
-            <p>Alpha</p>
+            <p>By Population</p>
           </div>
 
-          <div className='border-2 border-black m p-3 flex flex-col space-x-3'>
-            <p>Top 10</p>
-            <div className='flex space-x-6'>
-              <input type='checkbox' />
-              <p>By Population</p>
-            </div>
-
-            <div className='flex space-x-6'>
-              <input type='checkbox' />
-              <p>By Area</p>
-            </div>
-          </div>
-
-          <div className='border-2 border-black p-3 '>
-            <p>By Continent</p>
-            <select name="subregions">
-              <option value="test">test</option>
-            </select>
-          </div>
-
-          <div className='border-2 border-black p-3'>
-            <p>By Subregion</p>
-            <select name="subregions">
-              <option value="test">test</option>
-            </select>
+          <div className='flex space-x-6'>
+            <input type='checkbox' />
+            <p>By Area</p>
           </div>
         </div>
-        <button onClick={() => fetchData()}>Fetch Locations</button>
-        <div>{data.name}</div>
-        {data.length > 0 ? (
-          data.map((country, index) => (
-            <div key={index}>{country.name.common}</div>
-          ))
-        ) : (
-          <div>loading</div>
-        )}
+
+        <div className='border-2 border-black p-3 '>
+          <p>By Continent</p>
+          <select name="subregions">
+            <option value="test">test</option>
+            {uniqueRegions.length ? (
+              uniqueRegions.map((region, index) => (
+                <option key={index} className='text-black' value={region}>
+                  {region}
+                </option>
+              ))
+            ) : (
+              <option>No regions available</option>
+            )}
+          </select>
+        </div>
+
+        <div className='border-2 border-black p-3'>
+          <p>By Subregion</p>
+          <select name="subregions">
+            {uniqueSubregions.length ? (
+              uniqueSubregions.map((subregion, index) => (
+                <option key={index} className='text-black' value={subregion}>
+                  {subregion}
+                </option>
+              ))
+            ) : (
+              <option>No regions available</option>
+            )}
+          </select>
+        </div>
       </div>
 
+      <div className='countryContent'>
+        <Country data={data}/>
+      </div>
     </div>
   )
 }
 
 export default App
-
-
-/*
-
-"use client"
-//API Page Declaration
-import Image from "next/image";
-import { useState } from "react";
-
-export default function Page() {
-    const DATA_URL = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=5";
-    const [data, setData] = useState(null);
-    const [errorDisplay, setErrorDisplay] = useState(null)
-
-    async function fetchData() {
-        try {
-            const response = await fetch(DATA_URL);
-            console.log(response);
-            const data = await response.json()
-            setData(data)
-        } catch (error) {
-            console.log(error)
-            console.log("error message")
-            setErrorDisplay(<p>Error Message!</p>)
-        }
-        //add try catch function here
-    }
-
-    const DisplayMedia = () => {
-        if (data) {
-
-            const mediaList = [];
-            data.forEach((media, index) => {
-
-
-
-                mediaList.push(
-                    <li key={index} className="w-3/4 m-auto">
-                        <img className="rounded-lg m-auto mb-5 mt-10" src={media.url} />
-                        <h2 className="font-bold">{media.title}</h2>
-                        <p>{media.date}</p>
-                        <p>{media.explanation}</p>
-                    </li>
-                )
-            });
-
-            return (
-                <div className="">
-                    <ul >
-                        {mediaList}
-                    </ul>
-                </div>
-            )
-        } else {
-            return (
-                <div className="border-4 border-black p-4 mb-4">
-                    <ul >
-                        Nothing
-                    </ul>
-                </div>
-            )
-        }
-    }
-
-    return (
-        <div className="p-4 w-11/12 m-auto">
-            <header className="border-4 border-black p-4 mb-4 bg-yellow-300">
-                <h1>Welcome to my Images Page</h1>
-                <button
-                    className="border-neutral-200 bg-white bh-black px-6 rounded-md"
-                    onClick={() => fetchData()}
-                >Fetch Images</button>
-            </header>
-            Welcome to my API Page
-            <DisplayMedia />
-            {errorDisplay}
-        </div>
-    );
-}
-
-*/
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,43 +1,41 @@
-export default function Country({
-    dark,
-    name,
-    capital,
-    population,
-    flag,
-    region,
-    showDetails,
-    code,
-}) {
-
-    const showDetailsHandler = () => {
-        showDetails(code);
-    };
+export default function Country({ data }) {
 
     return (
         <>
-            <div
-                className={dark ? "country dark" : "country"}
-                onClick={showDetailsHandler}
-            >
-                <picture>
-                    <img src={flag} alt="Country Flag" />
-                </picture>
-                <section>
-                    <h2>{name}</h2>
-                    <p>
-                        <span className="country-info-highlight">Population:</span>
-                        {population}
-                    </p>
-                    <p>
-                        <span className="country-info-highlight"> Capital: </span>
-                        {capital}
-                    </p>
-                    <p>
-                        <span className="country-info-highlight">Region: </span>
-                        {region}
-                    </p>
-                </section>
-            </div>
+            {data.length ? (
+                data.map((country, index) => (
+                    <div key={index} className='countries m-1'>
+                        <div className='w-96'>
+                            <div className='flex bg-gray-500 font-bold text-2xl h-12'>
+                                <img src={country.flags.png} />
+                                <div>{country.name.common}</div>
+                            </div>
+                            <div className='bg-white text-black h-64 p-2'>
+                                <div><span className='font-bold'>Official Name:</span> {country.name.official}</div>
+                                <div><span className='font-bold'>Capital:</span> {country.capital}</div>
+                                <div><span className='font-bold'>Population:</span> {country.population}</div>
+                                <div><span className='font-bold'>Currency:</span>
+                                    {
+                                        country.currencies
+                                            ? Object.values(country.currencies).map((currency, i) =>
+                                                <div key={i}>{currency.name} ({currency.symbol})</div>
+                                            )
+                                            : 'N/A'
+                                    }
+                                </div>
+                                <div><span className='font-bold'>Area:</span> {country.area}</div>
+                                <div><span className='font-bold'>Subregion:</span> {country.subregion}</div>
+                                <div><span className='font-bold'>Continent:</span> {country.region}</div>
+                                <a className='text-blue-600 font-bold' href={country.maps.googleMaps}>
+                                    Show on Google Maps
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                ))
+            ) : (
+                <div>loading</div>
+            )}
         </>
     )
 }
